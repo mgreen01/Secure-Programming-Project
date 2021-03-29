62ee8b8170f00717f903eea3413be6159d380d7c
Morgan Green <MorganGreen0681@outlook.com>
2021-03-29 16:24:28 +0100

    ok

--- Code (MG)/time.cpp
+++ Code (MG)/time.cpp
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    cin
     string username;
-=======
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    string username;
->>>>>>> 6f3b9a044591a27edbc42bb111515c9ef7fc537e
 }
