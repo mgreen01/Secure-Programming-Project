5dbedb03c12cfb462c88d608ec94dd389e398468
Morgan Green <MorganGreen0681@outlook.com>
2021-04-05 15:37:08 +0100

    setters and getters done?

--- Code (MG)/Student.h
+++ Code (MG)/Student.h
@@ -1,15 +1,28 @@
+//
+// Author:  Morgan Green
+// Date created: 05/04/2021
+// Date modified: 05/04/2021
+// Title: Student Class Header
+// Description: creates the Student Class
 #include <string>
 #ifndef STUDENT_H
 #define STUDENT_H
 
 class Student {
+    private:
+        std::string student_username;
+        std::string student_password; 
     public:
+        Student();
         int student_ID;
         std::string student_name;
         std::string student_course;
-    private:
-        std::string student_username;
-        std::string student_password;
-    
+        void setPassword(std::string stu_pass){
+            student_password = stu_pass;
+        }
+        std::string getPassword () {
+            return student_password;
+        }
+
 };
 #endif
