efe876c3e8cbf5f53e0977cf42c1031b73d583bc
Morgan Green <MorganGreen0681@outlook.com>
2021-04-05 14:37:32 +0100

    added header file

---
+++ Code (MG)/Student.h
@@ -0,0 +1,14 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+class Student {
+    public:
+        int student_ID;
+        std::string student_name;
+        std::string student_course;
+    private:
+        std::string student_username;
+        std::string student_password;
+    
+}
+#endif
