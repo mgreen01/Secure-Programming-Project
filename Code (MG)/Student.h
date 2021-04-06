869b1b790abcd1944cf0e7a10abff6e0e453b58e
Morgan Green <MorganGreen0681@outlook.com>
2021-04-06 20:05:46 +0100

    main.cpp introducted

--- Code (MG)/Student.h
+++ Code (MG)/Student.h
@@ -4,22 +4,23 @@
 // Date modified: 05/04/2021
 // Title: Student Class Header
 // Description: creates the Student Class
-#include <iostream>
-#ifndef STUDENT_H
-#define STUDENT_H
-class Student {
-    private:
-        std::string student_username;
-        std::string student_password; 
-    public:
-        Student();
-        int student_ID;
-        std::string student_name;
-        std::string student_course;
-        std::string getPassword();
-        void setPassword(std::string stu_pass);
-        std::string getUsername(); 
-        void setUsername(std::string stu_username);
+//#include <iostream>
+//#ifndef STUDENT_H
+//#define STUDENT_H
+//class Student {
+//    private:
+//        std::string student_username;
+//        std::string student_password; 
+//    public:
+//        Student();
+//        int student_ID;
+//        std::string student_name;
+//        std::string student_course;
+//        std::string getPassword();
+//        void setPassword(std::string stu_pass);
+//        std::string getUsername(); 
+//        void setUsername(std::string stu_username);
 
-};
-#endif 
+//};
+//#endif //STUDENT_H#
+
