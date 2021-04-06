7a563acb9652cbd0173e754b01d22a447498c29c
Morgan Green <MorganGreen0681@outlook.com>
2021-04-06 21:11:35 +0100

    fart

--- Code (MG)/main.cpp
+++ Code (MG)/main.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+using namespace std;
+
+class Student {
+    int student_ID;
+    string student_name;
+    string student_course;
+    string student_username;
+    string student_password; 
+}
+
+class Lecturer {
+    int lecturer_ID;
+    string lecturer_name;
+    string lecturer_modules;
+    string 
+}
