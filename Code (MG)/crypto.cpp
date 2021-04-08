a780028d9bf0cd259089cd78e8e718453dc67f7d
Morgan Green <MorganGreen0681@outlook.com>
2021-04-08 15:29:11 +0100

    save

--- Code (MG)/crypto.cpp
+++ Code (MG)/crypto.cpp
@@ -1,1 +1,19 @@
 //This is a blank file
+#include <iostream>
+using namespace std;
+
+
+class Crypto {
+    string plain_password;
+    string encrypted_password;
+};
+
+class User {
+    string username;
+    int age;
+    string password;
+};
+
+int main () {
+    cout << 
+};
