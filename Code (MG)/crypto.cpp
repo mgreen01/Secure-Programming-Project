ce2ae57f0bdef0b48f3ff408d60f762d515a72a8
Morgan Green <MorganGreen0681@outlook.com>
2021-04-09 16:34:56 +0100

    lol

--- Code (MG)/crypto.cpp
+++ Code (MG)/crypto.cpp
@@ -12,6 +12,7 @@ class User {
     public:
     string username;
     int age;
+    int account_type;
     string password;
 };
 
@@ -19,5 +20,10 @@ int main () {
     cout << "Please enter a new username: " << endl;
     User new_user;
     cin >> new_user.username;
+    cout << "Please enter your age: " << endl;
+    cin >> new_user.age;
+    cout << "Please enter a new password: " << endl;
+    cin >> new_user.password;
+
     return 0;
 };
